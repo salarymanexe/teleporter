7e040cb413a55b569e3b90e3e7c7c12b2f7d0952
Dan Yeomans <danyeomans98@gmail.com>
2016-04-23 18:50:27 -0400

    1.5.0 version ready

--- src/main/java/net/dyeo/teleporter/Reference.java
+++ src/main/java/net/dyeo/teleporter/Reference.java
@@ -6,7 +6,7 @@ package net.dyeo.teleporter;
 public final class Reference {
 
 	public static final String MODID = "teleporter";
-	public static final String VERSION = "1.4.6";
+	public static final String VERSION = "1.5.0";
 
 	//public static boolean useCustomTextures = false;
 	
