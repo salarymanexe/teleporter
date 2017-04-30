74eb5a9343852f193d0b90396541aeef4d95db6c
Chris Porter <github@crazysnailboy.net>
2017-04-30 23:55:54 +0100

    version bump, added update check

--- src/main/java/net/dyeo/teleporter/TeleporterMod.java
+++ src/main/java/net/dyeo/teleporter/TeleporterMod.java
@@ -11,13 +11,14 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 
-@Mod(modid = TeleporterMod.MODID, name = TeleporterMod.NAME, version = TeleporterMod.VERSION)
+@Mod(modid = TeleporterMod.MODID, name = TeleporterMod.NAME, version = TeleporterMod.VERSION, updateJSON = TeleporterMod.UPDATEJSON)
 public class TeleporterMod
 {
 
 	public static final String MODID = "teleporter";
 	public static final String NAME = "Vanilla-Inspired Teleporters";
-	public static final String VERSION = "1.6.0";
+	public static final String VERSION = "${version}";
+	public static final String UPDATEJSON = "https://raw.githubusercontent.com/crazysnailboy/VanillaTeleporter/master/update.json";
 
 	private static final String CLIENT_PROXY_CLASS = "net.dyeo.teleporter.proxy.ClientProxy";
 	private static final String SERVER_PROXY_CLASS = "net.dyeo.teleporter.proxy.CommonProxy";
