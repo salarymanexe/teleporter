aea9f0aa7be5a1fe086d53672f2239b64abde7c7
Dan-Home <danyeomans98@gmail.com>
2019-07-22 15:00:00 -0400

    Re-enable diamondless config option

--- src/main/java/net/dyeo/teleporter/common/config/ModConfiguration.java
+++ src/main/java/net/dyeo/teleporter/common/config/ModConfiguration.java
@@ -15,7 +15,7 @@ public final class ModConfiguration
 
 	public static final String CATEGORY_SOUNDS = "sounds";
 
-	//public static boolean useDiamonds = true;
+	public static boolean useDiamonds = true;
 	//public static int numTeleporters = 1;
 	public static boolean teleportPassiveMobs = true;
 	public static boolean teleportHostileMobs = true;
@@ -36,7 +36,9 @@ public final class ModConfiguration
 
 
 		config.addCustomCategoryComment(Configuration.CATEGORY_GENERAL, "Vanilla-Inspired Teleporters Version " + TeleporterMod.VERSION + " Configuration");
-		//Property propUseDiamonds = config.get(Configuration.CATEGORY_GENERAL, "useDiamonds", useDiamonds, "If false, removes diamonds from the crafting recipe and replaces them with quartz blocks.\nDefault is true");
+		Property propUseDiamonds = config.get(Configuration.CATEGORY_GENERAL, "useDiamonds", useDiamonds, "If false, removes diamonds from the crafting recipe and replaces them with iron (regular) or obsidian (ender) blocks.\n" +
+				"When using the /recipe command, the diamond recipes are teleporter:regular/teleporter , teleporter:regular/ender_teleporter and the diamondless recipes are teleporter:simple/teleporter , teleporter:simple/ender_teleporter\n" +
+				"Default is true");
 		//Property propNumTeleporters = config.get(Configuration.CATEGORY_GENERAL, "numTeleporters", numTeleporters, "Specifies the number of teleporters created with a single recipe.\nDefault is 1");
 		Property propTeleportPassiveMobs = config.get(Configuration.CATEGORY_GENERAL, "teleportPassiveMobs", teleportPassiveMobs, "Specifies whether or not passive mobs can go through teleporters.\nDefault is true");
 		Property propTeleportHostileMobs = config.get(Configuration.CATEGORY_GENERAL, "teleportHostileMobs", teleportHostileMobs, "Specifies whether or not hostile mobs can go through teleporters.\nDefault is true");
@@ -48,7 +50,7 @@ public final class ModConfiguration
 
 
 		List<String> propOrderGeneral = new ArrayList<String>();
-		//propOrderGeneral.add(propUseDiamonds.getName());
+		propOrderGeneral.add(propUseDiamonds.getName());
 		//propOrderGeneral.add(propNumTeleporters.getName());
 		propOrderGeneral.add(propTeleportPassiveMobs.getName());
 		propOrderGeneral.add(propTeleportHostileMobs.getName());
@@ -60,7 +62,7 @@ public final class ModConfiguration
 		propOrderSounds.add(propSoundEffectTeleporterError.getName());
 		config.setCategoryPropertyOrder(ModConfiguration.CATEGORY_SOUNDS, propOrderSounds);
 
-		//useDiamonds = propUseDiamonds.getBoolean();
+		useDiamonds = propUseDiamonds.getBoolean();
 		//numTeleporters = propNumTeleporters.getInt();
 		teleportPassiveMobs = propTeleportPassiveMobs.getBoolean();
 		teleportHostileMobs = propTeleportHostileMobs.getBoolean();
