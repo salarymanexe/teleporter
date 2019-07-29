69c6e1974c499cc3c9f4b66f75b6b979fef94f72
Dan-Home <danyeomans98@gmail.com>
2019-07-29 01:04:16 -0400

    Fix tooltip rendering

--- src/main/java/net/dyeo/teleporter/client/gui/inventory/GuiTeleporter.java
+++ src/main/java/net/dyeo/teleporter/client/gui/inventory/GuiTeleporter.java
@@ -43,4 +43,11 @@ public class GuiTeleporter extends GuiContainer
 		this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
 	}
 
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks)
+	{
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX,mouseY,partialTicks);
+		this.renderHoveredToolTip(mouseX,mouseY);
+	}
 }
